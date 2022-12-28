c05a33ffb71876068a0e5056298cb80cf3834f4e
Michael Rebello <me@michaelrebello.com>
2022-12-28 10:33:44 -0800

    Create file header template & add to all `.swift` files

--- Connect/Interfaces/Code.swift
+++ Connect/Interfaces/Code.swift
@@ -1,3 +1,19 @@
+//
+// Copyright 2022 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 /// Indicates a status of an RPC.
 /// The zero code in gRPC is OK, which indicates that the operation was a success.
 public enum Code: Int, CaseIterable, Equatable {
