3b7fca93b01a1bdd07427d962965c8469a19d346
Michael Rebello <me@michaelrebello.com>
2022-12-29 10:47:40 -0800

    more

---
+++ ConnectTests/ConnectErrorTests.swift
@@ -0,0 +1,72 @@
+//
+// Copyright 2022 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+@testable import Connect
+import Foundation
+import Generated
+import SwiftProtobuf
+import XCTest
+
+final class ConnectErrorTests: XCTestCase {
+    func testDeserializingFullErrorAndUnpackingDetails() throws {
+        // Example error from https://connect.build/docs/protocol/#error-end-stream
+        let expectedDetails = Grpc_Testing_SimpleResponse.with { $0.hostname = "foobar" }
+        let errorData = try self.errorData(expectedDetails: expectedDetails)
+        let error = try JSONDecoder().decode(ConnectError.self, from: errorData)
+        XCTAssertEqual(error.code, .unavailable)
+        XCTAssertEqual(error.message, "overloaded: back off and retry")
+        XCTAssertNil(error.exception)
+        XCTAssertEqual(error.details.count, 1)
+        XCTAssertEqual(error.unpackedDetails(), expectedDetails)
+        XCTAssertTrue(error.metadata.isEmpty)
+    }
+
+    func testDeserializingSimpleError() throws {
+        let errorDictionary = [
+            "code": "unavailable",
+        ]
+        let errorData = try JSONSerialization.data(withJSONObject: errorDictionary)
+        let error = try JSONDecoder().decode(ConnectError.self, from: errorData)
+        XCTAssertEqual(error.code, .unavailable)
+        XCTAssertNil(error.message)
+        XCTAssertNil(error.exception)
+        XCTAssertTrue(error.details.isEmpty)
+        XCTAssertNil(error.unpackedDetails() as Grpc_Testing_SimpleResponse?)
+        XCTAssertTrue(error.metadata.isEmpty)
+    }
+
+    func testDeserializingEmptyDictionaryFails() throws {
+        let errorData = try JSONSerialization.data(withJSONObject: [String: Any]())
+        XCTAssertThrowsError(try JSONDecoder().decode(ConnectError.self, from: errorData))
+    }
+
+    // MARK: - Private
+
+    private func errorData(expectedDetails: SwiftProtobuf.Message) throws -> Data {
+        let dictionary: [String: Any] = [
+            "code": "unavailable",
+            "message": "overloaded: back off and retry",
+            "details": [
+                [
+                    "type": type(of: expectedDetails).protoMessageName,
+                    "value": try expectedDetails.serializedData().base64EncodedString(),
+                    "debug": ["retryDelay": "30s"],
+                ],
+            ],
+        ]
+        return try JSONSerialization.data(withJSONObject: dictionary)
+    }
+}
