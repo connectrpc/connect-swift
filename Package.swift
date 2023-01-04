840726c7a37a3061f3961c5ff1a15b3ee789f87b
Michael Rebello <me@michaelrebello.com>
2023-01-03 17:08:55 -0800

    Update licenses to include 2023 (#48)

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 // swift-tools-version:5.6
 
-// Copyright 2022 Buf Technologies, Inc.
+// Copyright 2022-2023 Buf Technologies, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
