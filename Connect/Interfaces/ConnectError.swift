971b570e280d0b5b4f61f5a78a50089cd23f0d04
Michael Rebello <me@michaelrebello.com>
2023-01-13 13:44:43 -0800

    Return a list of typed errors from `ConnectError` (#60)
    This more closely resembles the [connect-web
    implementation](https://github.com/bufbuild/connect-web/blob/f0c3511927f9178f5e4fab45b1e79c3f8dc2867a/packages/connect-web/src/connect-error.ts#L95)
    and allows consumers to access all of the errors if multiple of the same
    type are specified (rather than only the first). Also makes the function
    name a bit clearer/relevant.

--- Connect/Interfaces/ConnectError.swift
+++ Connect/Interfaces/ConnectError.swift
@@ -29,16 +29,22 @@ public struct ConnectError: Swift.Error {
     /// Additional key-values that were provided by the server.
     public private(set) var metadata: Headers
 
-    /// Unpacks values from `self.details` and returns the first matching error, if any.
+    /// Unpacks values from `self.details` and returns all matching errors.
     ///
-    /// - returns: The unpacked typed error details, if available.
-    public func unpackedDetails<Output: SwiftProtobuf.Message>() -> Output? {
-        for detail in self.details where detail.type == Output.protoMessageName {
-            if let decoded = detail.payload.flatMap({ try? Output(serializedData: $0) }) {
-                return decoded
+    /// Any decoding errors are ignored, and the detail will simply be omitted from the list.
+    ///
+    /// To access only the first error of a specific type:
+    /// `let unpackedError: MyError? = error.unpackedDetails().first`
+    ///
+    /// - returns: The matching unpacked typed error details.
+    public func unpackedDetails<Output: SwiftProtobuf.Message>() -> [Output] {
+        return self.details.compactMap { detail -> Output? in
+            guard detail.type == Output.protoMessageName else {
+                return nil
             }
+
+            return detail.payload.flatMap { try? Output(serializedData: $0) }
         }
-        return nil
     }
 
     /// Error details are sent over the network to clients, which can then work with
