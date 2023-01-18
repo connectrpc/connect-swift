b0b426585dda9df1f34224cff09767266c7a3299
Michael Rebello <me@michaelrebello.com>
2023-01-18 15:13:20 -0800

    Update HTTPResponse.swift

--- Libraries/Connect/Interfaces/HTTPResponse.swift
+++ Libraries/Connect/Interfaces/HTTPResponse.swift
@@ -17,6 +17,7 @@ import Foundation
 /// Unary HTTP response received from the server.
 public struct HTTPResponse {
     /// The status code of the response.
+    /// See https://connect.build/docs/protocol/#error-codes for more info.
     public let code: Code
     /// Response headers specified by the server.
     public let headers: Headers
