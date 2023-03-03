0b8fa0dfe174ba62342784f196e23999c0472aed
Michael Rebello <me@michaelrebello.com>
2023-03-02 17:16:51 -0800

    Bump SwiftProtobuf to 1.21.0 (#103)
    https://github.com/apple/swift-protobuf/releases/tag/1.21.0

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-protobuf.git",
-            from: "1.20.3"
+            from: "1.21.0"
         ),
     ],
     targets: [
