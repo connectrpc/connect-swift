db88acf5be11730c7979d4434839a8b13362a757
Michael Rebello <me@michaelrebello.com>
2023-09-28 16:42:57 -0700

    wip

--- Libraries/Connect/Interfaces/Interceptors/Interceptor.swift
+++ Libraries/Connect/Interfaces/Interceptors/Interceptor.swift
@@ -12,12 +12,48 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import Foundation
-
 /// A type that can be registered with clients as a way to observe and/or alter outbound requests
 /// and inbound responses.
 ///
 /// Interceptors are expected to be instantiated **once per request or stream**.
+///
+/// Each interceptor has the opportunity to perform asynchronous work before passing a potentially
+/// altered value to the next interceptor in the chain. When the end of the chain is reached, the
+/// final value is passed to the networking client where it is sent to the server or to the calling
+/// client.
+///
+/// Interceptors are closure-based, and are passed both the current value and a closure which
+/// should be called to resume the interceptor chain. They must call this closure to continue.
+/// New values may be passed to a given interceptor even though it has not yet resumed the chain,
+/// for example:
+/// - Request is sent
+/// - Response headers are received, and interceptor pauses the chain while processing
+/// - First chunk of streamed data is received, and the interceptor receives this value immediately
+/// - Interceptor is expected to resume headers first, followed by data
+///
+/// Interceptors can also be written using `async/await` by incorporating a `Task`. For example:
+///
+///```
+/// final class AsyncInterceptor: Interceptor, Sendable {
+///    func unaryFunction() -> UnaryFunction {
+///        return .init { request, proceed in
+///            Task {
+///                proceed(await self.handleRequest(request))
+///            }
+///        } responseFunction: { response, proceed in
+///            Task {
+///                proceed(await self.handleUnaryResponse(response))
+///            }
+///        } responseMetricsFunction: { metrics, proceed in
+///            Task {
+///                proceed(await self.handleUnaryResponseMetrics(metrics))
+///            }
+///        }
+///    }
+///
+///    func streamFunction() -> StreamFunction {...}
+/// }
+/// ```
 public protocol Interceptor {
     /// Invoked when a unary request is started. Provides a set of closures that will be called
     /// as the request progresses, allowing the interceptor to read/alter request/response data.
@@ -29,7 +65,7 @@ public protocol Interceptor {
     /// as the stream progresses, allowing the interceptor to read/alter request/response data.
     ///
     /// NOTE: Some closures may be called multiple times as the stream progresses
-    /// (for example, as data is sent/received over the stream).
+    /// (for example, as data chunks are sent/received over a bidirectional stream).
     ///
     /// A guarantee is provided that each data chunk will contain 1 full message
     /// (for Connect and gRPC, this includes the prefix and message
