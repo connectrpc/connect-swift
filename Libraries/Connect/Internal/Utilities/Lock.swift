734f9465462a38666c269d80c20006b61281d1a2
Michael Rebello <me@michaelrebello.com>
2024-05-28 17:18:18 -0700

    Add client-side timeout enforcement (#265)
    Adds support for triggering client-side timeouts if the client is
    configured to do so. Resolves several previously opted-out conformance
    tests.
    
    Resolves https://github.com/connectrpc/connect-swift/issues/267.
    
    ---------
    
    Signed-off-by: Michael Rebello <me@michaelrebello.com>

--- Libraries/Connect/Internal/Utilities/Lock.swift
+++ Libraries/Connect/Internal/Utilities/Lock.swift
@@ -15,7 +15,7 @@
 import Foundation
 
 /// Internal implementation of a lock. Wraps usage of `os_unfair_lock`.
-final class Lock: Sendable {
+final class Lock: @unchecked Sendable {
     private let underlyingLock: UnsafeMutablePointer<os_unfair_lock>
 
     init() {
