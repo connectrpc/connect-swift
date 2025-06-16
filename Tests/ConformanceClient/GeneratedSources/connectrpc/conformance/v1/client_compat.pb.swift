2208720c56405bfa7086b1f9f5e24a3d1ce6449e
Mike Fridman <mfridman@buf.build>
2025-06-16 12:10:11 -0400

    generate code
    Signed-off-by: Mike Fridman <mfridman@buf.build>

--- Tests/ConformanceClient/GeneratedSources/connectrpc/conformance/v1/client_compat.pb.swift
+++ Tests/ConformanceClient/GeneratedSources/connectrpc/conformance/v1/client_compat.pb.swift
@@ -583,15 +583,11 @@ extension Connectrpc_Conformance_V1_ClientCompatRequest: SwiftProtobuf.Message, 
     var _cancel: Connectrpc_Conformance_V1_ClientCompatRequest.Cancel? = nil
     var _rawRequest: Connectrpc_Conformance_V1_RawHTTPRequest? = nil
 
-    #if swift(>=5.10)
       // This property is used as the initial default value for new instances of the type.
       // The type itself is protecting the reference to its storage via CoW semantics.
       // This will force a copy to be made of this reference when the first mutation occurs;
       // hence, it is safe to mark this as `nonisolated(unsafe)`.
       static nonisolated(unsafe) let defaultInstance = _StorageClass()
-    #else
-      static let defaultInstance = _StorageClass()
-    #endif
 
     private init() {}
 
